feb979847f87e029436f7913578b21ab4dcf7246
MeVsTheVoices <MeVsTheVoices@gmail.com>
2023-10-09 16:57:17 -0700

    added comments

--- Assignment-5/Assignment-5.bash
+++ Assignment-5/Assignment-5.bash
@@ -7,15 +7,21 @@ encrypt() {
 
     for ((i=0; i<${#message}; i++)); do
         char="${message:$i:1}"
+	# We only want to transform alphabetic characters
         if [[ $char =~ [A-Za-z] ]]; then
             ascii_val=$(printf "%d" "'$char")
             if [[ $char =~ [A-Z] ]]; then
+		# To explain (ascii_val - 65 + offset) gives us (ascii_val - 'A' + offset)
+		# We then mod 26 (26 characters in the alphabet), then add back 'A'
                 ascii_val=$(((ascii_val - 65 + offset) % 26 + 65))
             else
+		# Likewise, we operate similarly, except using 'a' = 97 as our starting point
                 ascii_val=$(((ascii_val - 97 + offset) % 26 + 97))
             fi
+	    # Output three width octal
             encrypted+="$(printf "\\$(printf '%03o' "$ascii_val")")"
         else
+	    # If we didn't encounter alphabetic, just append
             encrypted+="$char"
         fi
     done
@@ -23,11 +29,9 @@ encrypt() {
     echo "$encrypted"
 }
 
-# Read the message and offset from the user
 read -p "Enter the message: " message
 read -p "Enter the offset: " offset
 
-# Encrypt the message using the custom offset
 encrypted_message=$(encrypt "$message" "$offset")
 echo "Encrypted message: $encrypted_message"
 
