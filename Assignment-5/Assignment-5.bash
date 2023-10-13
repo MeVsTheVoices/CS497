9bcc016fb3b7c6821f62e08fb9047f6589aa979e
MeVsTheVoices <MeVsTheVoices@gmail.com>
2023-10-13 13:48:34 -0700

    changes to assignment 5

--- Assignment-5/Assignment-5.bash
+++ Assignment-5/Assignment-5.bash
@@ -1,34 +1,17 @@
 #!/bin/bash
 
 encrypt() {
-	local message="$1"
+	local encrypted="$1"
 	local offset="$2"
-	local encrypted=""
+	local decrypted=""
 
-	# I was tempted to use tr for this, but, this is a good excuse to
-	# demonstrate basic looping and string manipulation in bash
-	for ((i=0; i<${#message}; i++)); do
-		char="${message:$i:1}"
-		# We only want to transform alphabetic characters
-		if [[ $char =~ [A-Za-z] ]]; then
-			ascii_val=$(printf "%d" "'$char")
-			if [[ $char =~ [A-Z] ]]; then
-				# To explain (ascii_val - 65 + offset) gives us (ascii_val - 'A' + offset)
-				# We then mod 26 (26 characters in the alphabet), then add back 'A'
-				ascii_val=$(((ascii_val - 65 + offset) % 26 + 65))
-			else
-				# Likewise, we operate similarly, except using 'a' = 97 as our starting point
-				ascii_val=$(((ascii_val - 97 + offset) % 26 + 97))
-			fi
-			# Output three width octal
-			encrypted+="$(printf "\\$(printf '%03o' "$ascii_val")")"
-		else
-		# If we didn't encounter alphabetic, just append
-		encrypted+="$char"
-	       fi
-	done
+	upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	lower="abcdefghijklmnopqrstuvwxyz"
+	# The logic here goes as follows, start at offset, and go to the end of the string
+	# Then, go from the beginning of the string to offset
+	decrypted=$(echo "$encrypted" | tr "\[${upper:$offset}${upper::$offset}${lower:$offset}${lower::$offset}\]" "[$upper$lower]")
 
-	echo "$encrypted"
+	echo "$decrypted"
 }
 
 decrypt() {
